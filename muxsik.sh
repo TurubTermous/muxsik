1be19c6d3198f330221e5934a67853737c60c600
TurubTermous <97165381+TurubTermous@users.noreply.github.com>
2022-01-07 23:03:03 +0700

    Add files via upload

---
+++ muxsik.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo -e "\e[92m"
+cat README.md
+echo -e "\e[93m                            <<<<<<<<<<<<<<🚫>>>>>>>>>>>>>>";
+echo -e "\e[92m                            🚫AUTHOR : ~~> TURUB_TERMOUS🚫";
+echo -e "\e[93m                            <<<<<<<<<<<<<<🚫>>>>>>>>>>>>>>";
+printf "\n"
+printf "\n"
+echo -e "\e[92m"
+read -p "$.Click [enter] ~~> " pil;
+echo -e "\e[0m"
+cd mp3
+bash logsik.sh
+play '#anonymous_#hacker_Epic_Anonymous_hacker_music💻Let'\''s_Heal⌨️(256k).mp3'
+bash logsik.sh
+play  Ambitions_Banger_Trap_Beat_Aggressive_Instrumental_Tech_N9ne_Type.mp3
+bash logsik.sh
+play 'DJ_Anonymous_-_2012_Remix_Electro_House_-_Haters(256k).mp3'
+bash logsik.sh
+play 'Hard_Epic_Orchestra_Underground_RAP_INSTRUMENTAL_HIPHOP_BEAT_-_Anonymous(256k).mp3'
+bash logsik.sh
+play 'ITJ_Ibnu_The_Jenggot_Adzan(128k).m4a'
+bash logsik.sh
+play 'In_The_End_[Official_HD_Music_Video]_-_Linkin_Park(128k).m4a'
+bash logsik.sh
+play Mihaita_Piticu_Ploua_XZEEZ_Remix_FAST_FURIOUS_Chase_SceneMP3_128K.mp3
+bash logsik.sh
+play 'We_Are_Anonymous_-_[HARD]_Remix(256k).mp3'
